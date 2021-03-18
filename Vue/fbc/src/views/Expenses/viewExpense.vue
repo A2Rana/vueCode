cffb2f9cf6f5db964b6a1bfe08a815063d24b723
vishakha0618 <vtyagi@ex2india.com>
2021-03-18 08:51:38 +0530

    View Expense done

--- Vue/fbc/src/views/Expenses/viewExpense.vue
+++ Vue/fbc/src/views/Expenses/viewExpense.vue
@@ -11,33 +11,49 @@
 <script>
     import Table from '@/components/Table.vue';
     export default {
-    name: 'View Expense',
-    components: {
-        Table
-    },
-    data() {
-        return {
-        data: [],
-        tableHeaders:[],
-        yearArray:[],
-        selectedYear:null
-        };
-    },
-    methods: {
-        async getData(param='') {
-            const response = await fetch('https://fbc.exitest.com/overhead' + param);
-            if (response.ok) {
-                const data = await response.json();
-                return data;
-            } else {
-                return console.log('HTTP-Error: ' + response.status);
+        name: 'View Expense',
+        components: {
+            Table
+        },
+        data() {
+            return {
+                data: [],
+                tableHeaders:[],
+                yearArray:[],
+                selectedYear:null,
+                url:'https://fbc.exitest.com/totalExpense'
+            };
+        },
+        methods: {
+            async getData(param='') {
+                const response = await fetch(this.url + param);
+                if (response.ok) {
+                    const data = await response.json();
+                    return data;
+                } else {
+                    return console.log('HTTP-Error: ' + response.status);
+                }
+            },
+            async changeYear(event) {
+                this.selectedYear = (event.target.options[event.target.options.selectedIndex]).value;
+                this.data = Object.values(await this.getData( '/'+this.selectedYear));
             }
         },
-        async viewExpense() {
-        this.data = Object.values(await this.getData(this.url));
-        this.tableHeaders = Object.keys(this.data[0]);
-        this.data.forEach(item => this.yearArray.push(item.Year));
+        mounted: async function() {
+            this.data = Object.values(await this.getData());
+            if (this.data.length){
+                this.tableHeaders = Object.keys(this.data[0]);
+                this.data.forEach(item => this.yearArray.push(item.Year));
+
+            }else{
+                this.tableHeaders = ['Data is not present!'];
+            }
         }
-    }
     };
 </script>
+
+<style scoped>
+    table{
+        display:inline-block;
+    }
+</style>
