60488b673e278f2f9ee9b2c54ad9337f9483e7ca
vishakha0618 <vtyagi@ex2india.com>
2021-03-20 19:19:07 +0530

    Calculate Expense Working

--- Vue/fbc/src/views/Expenses/calculateExpense.vue
+++ Vue/fbc/src/views/Expenses/calculateExpense.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2 v-if="show"> Estimated Expense for {{year}} at profit percentange {{per}}%: {{data[0]}}</h2>
+    <Table v-if="show" v-bind:tableHeaders="tableHeaders" v-bind:data="data"></Table>
     <input type="text" id="year" name="Year" required="" placeholder="Enter Year.." />
     <br />
     <input type="text" id="profit" name="per" required="" placeholder="Profit Percentange.." />
@@ -9,24 +9,23 @@
 </template>
 
 <script>
+    import Table from '../../components/Table.vue';
 
 export default {
     name: 'Calculate Expense',
+    components:{
+        Table
+    },
     data() {
         return {
             data: [],
-            year :2020,
-            per:5,
-            show:false
+            show:false,
+            tableHeaders:[]
         };
     },
     methods: {
         async getData(year,per) {
-            if(year){
-                this.year = year;
-                this.per = per;
-            }
-            const response = await fetch(`https://fbc.exitest.com/employee/expenseDetails/year=${this.year}/profit=${this.per}`);
+            const response = await fetch(`https://fbc.exitest.com/employee/expenseDetails/year=${year}/profit=${per}`);
             if (response.ok) {
                 const data = await response.json();
                 return Object.values(data);
@@ -37,7 +36,8 @@ export default {
         async doCalculations() {
             const year = parseInt(document.getElementById('year').value,10);
             const per = parseInt(document.getElementById('profit').value,10);
-            this.data = await this.getData(year,per);
+            this.data[0] = await this.getData(year,per);
+            this.tableHeaders = ['Total Expense'];
             this.show = true;
         },
     },
