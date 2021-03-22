822e58b38cce03ca3292c6ef40c80158885e719c
Aakash <arana@ex2india.com>
2021-03-22 10:40:01 +0530

    update benefit, overhead fixed

--- Vue/fbc/src/components/updateFacility.vue
+++ Vue/fbc/src/components/updateFacility.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <form action="POST" target="http://localhost/3000/benefit/update">
+        <form method="POST" action="http://localhost:3000/benefit/update">
             <label for="FaciltyId">Facility ID</label>
-            <input type="text" v-bind:value="data.FacilityID" required readonly />
+            <input type="text" name="FacilityId" v-bind:value="data.FacilityID" required readonly />
             <label for="FacilityName">Facility Name</label>
-            <input type="text" v-bind:value="data.Name" required />
+            <input type="text" name="FacilityName" v-bind:value="data.Name" required />
             <label for="Description">Facility Description</label>
-            <textarea wrap="off" v-bind:value="data.Description" required cols="100" rows="10"></textarea>
+            <textarea name="FacilityDescription" wrap="off" v-bind:value="data.Description" required cols="100" rows="10"></textarea>
             <label for="ActiveStatus">Status</label>
-            <select type="text" v-bind:value="data.ActiveStatus ? 1 : 0" required>
+            <select type="text" name="IsActive" v-bind:value="data.ActiveStatus ? 1 : 0" required>
                 <option value="1">Active</option>
                 <option value="0">Terminated</option>
             </select>
-            <input type="submit" />
+            <input type="submit" value="Submit" /><br />
         </form>
     </div>
 </template>
