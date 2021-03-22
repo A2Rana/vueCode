97c83f9e98373afe1f6e671297772ef311a60788
vishakha0618 <vtyagi@ex2india.com>
2021-03-22 13:01:05 +0530

    Expense Vendor Done

--- Vue/fbc/src/components/Filter.vue
+++ Vue/fbc/src/components/Filter.vue
@@ -2,6 +2,7 @@
     <div>
         <h2>{{ msg }}</h2>
         <input v-model.lazy="inputID" :placeholder="message" type="number" required />
+        <input v-if="route" v-model.lazy="inputYear" placeholder="Enter year" type="number" required />
         <button @click="getID()" type="button">Go!</button>
         <button v-if="showButton" @click="back()" type="button">Back</button>
     </div>
@@ -19,38 +20,45 @@ export default {
         return {
             mainParam: '/id=',
             inputID: '',
+            inputYear:'',
             showButton:false,
+            showYear:false
         };
     },
     methods: {
         async getID() {
             if(this.inputID === ''){
                 window.alert('Please enter the Input ID!');
-            }
-            else{
-            if (this.msg.indexOf('year') >= 0 && this.msg.indexOf('Benefit') >= 0) {
-                this.mainParam = '/expense/year=';
-            }
-            if (this.msg.indexOf('year') >= 0 && this.msg.indexOf('Overhead') >= 0) {
-                this.mainParam = '/year=';
-            }
-            if (this.msg.indexOf('Get') >= 0 && this.msg.indexOf('Benefits') >= 0) {
-                this.mainParam = '/employee/';
-            }
-            if (this.msg.indexOf('expenses') >= 0 && this.msg.indexOf('Employee') >= 0) {
-                this.mainParam = '';
-            }
-            const param = this.inputID ? this.mainParam + this.inputID : this.inputID;
-            this.$parent.$data.data = Object.values(await this.$parent.getData(param));
-            if (this.$parent.$data.data.length) {
-                this.$parent.$data.tableHeaders = Object.keys(this.$parent.$data.data[0]);
-            } else {
-                this.$parent.$data.tableHeaders = ['Data is not present!'];
-            }
-            if(this.route != 'http://localhost:8080/expenseEmployee'){
-                this.showButton=true;
-            }
-            this.inputID = '';
+            }else{
+                if(this.route == 'http://localhost:8080/expenseEmployee' || this.route == 'http://localhost:8080/expenseVendor'){
+                        if(this.inputYear===''){
+                        window.alert('Please enter the Year!');
+                    }
+                }
+                if (this.msg.indexOf('year') >= 0 && this.msg.indexOf('Benefit') >= 0) {
+                    this.mainParam = '/expense/year=';
+                }
+                if (this.msg.indexOf('year') >= 0 && this.msg.indexOf('Overhead') >= 0) {
+                    this.mainParam = '/year=';
+                }
+                if (this.msg.indexOf('Get') >= 0 && this.msg.indexOf('Benefits') >= 0) {
+                    this.mainParam = '/employee/';
+                }
+                if (this.msg.indexOf('expenses') >= 0 && this.msg.indexOf('Employee') >= 0) {
+                    this.mainParam = '';
+                }
+                const param = this.inputID ? this.mainParam + this.inputID : this.inputID;
+                this.$parent.$data.data = Object.values(await this.$parent.getData(param,this.inputYear));
+                if (this.$parent.$data.data.length) {
+                    this.$parent.$data.tableHeaders = Object.keys(this.$parent.$data.data[0]);
+                } else {
+                    this.$parent.$data.tableHeaders = ['Data is not present!'];
+                }
+                if(this.route != 'http://localhost:8080/expenseEmployee' && this.route != 'http://localhost:8080/expenseVendor'){
+                    this.showButton=true;
+                }
+                this.inputID = '';
+                this.inputYear='';
             }
         },
         async back(){
--- Vue/fbc/src/views/Vendors/expenseVendor.vue
+++ Vue/fbc/src/views/Vendors/expenseVendor.vue
@@ -1,8 +1,8 @@
 <template>
     <h1>Expenses for Vendors</h1>
-    <h2>Vendor Id : {{id}}</h2>
-    <Table v-bind:tableHeaders="tableHeaders" v-bind:data="data"></Table>
-    <Filter v-bind:msg="msg" v-bind:message="message" />
+    <h2 v-if="show">Vendor Id : {{id}}</h2>
+    <Table v-if="show" v-bind:tableHeaders="tableHeaders" v-bind:data="data"></Table>
+    <Filter v-bind:msg="msg" v-bind:message="message" v-bind:route="route"/>
 </template>
 
 <script>
@@ -22,31 +22,28 @@ export default {
             data: [],
             tableHeaders: [],
             ctcdata: [],
-            id:Number
+            id:Number,
+            year:Number,
+            show:false,
+            route:'http://localhost:8080/expenseVendor'
         };
     },
     methods: {
-        async getData(vID, year = 2021) {
-            this.id=vID;
-            const response = await fetch(`https://fbc.exitest.com/benefit/employee/${vID}/expense`);
-            const ctcresponse = await fetch(`https://fbc.exitest.com/employee/compensation/year=${year}/id=${vID}`);
+        async getData(vID, year) {
+            this.id=vID.slice(4);
+            this.year=year;
+            const response = await fetch(`https://fbc.exitest.com/vendor/${this.id}/earning/${this.year}`);
             if (response.ok) {
-                const Data = await response.json();
-                this.ctcdata = await ctcresponse.json();
-                if (Data.length === 1 && Object.values(Data[0])[0] === null && this.ctcdata.length === 0) {
-                    return ['Data is not present'];
-                }
-                if (!Data.length && !this.ctcdata.length) {
-                    return ['Data is not present!'];
-                }
-                Data.push({ Name: Object.keys(this.ctcdata[0])[0], ExpenseAmount: Object.values(this.ctcdata[0])[0] });
-                return Data;
+                const data = await response.json();
+                this.data = Object.values(data);
+                this.show=true;
+                return data;
+            } else {
+                return console.log('HTTP-Error: ' + response.status);
             }
-            return console.log('HTTP-Error: ' + response.status);
         },
     },
     async mounted() {
-        this.data = Object.values(await this.getData(3));
         if (this.data.length) {
             this.tableHeaders = Object.keys(this.data[0]);
         } else {
